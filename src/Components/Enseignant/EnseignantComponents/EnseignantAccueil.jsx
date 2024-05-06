7689b5e5f99d3956eb6695533035e328c6c60573
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2024-05-06 13:29:19 +0100

    new update

--- src/Components/Enseignant/EnseignantComponents/EnseignantAccueil.jsx
+++ src/Components/Enseignant/EnseignantComponents/EnseignantAccueil.jsx
@@ -1,3 +1,9 @@
 export default function EnseignantAccueil() {
-    
+    return (
+        <div>
+            <div className="mt-8 grid lg:grid-cols-3 gap-7 sm:grid-cols-2">
+
+            </div>
+        </div>
+    )
 }
