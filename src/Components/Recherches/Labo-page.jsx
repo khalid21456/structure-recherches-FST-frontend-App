5bc011a683e09e10a7fd799249b6d21f56580b78
khalid21456 <khalidedaoudi8@gmail.com>
2024-06-04 19:26:21 +0100

    add themes to labo

--- src/Components/Recherches/Labo-page.jsx
+++ src/Components/Recherches/Labo-page.jsx
@@ -1,61 +1,68 @@
-import React, { useState, useEffect,useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import LaboMembre from "./LaboMembre";
 import DoctorantLabo from "./Doctorant-labo";
 import PublicationCard from "../Publications/PublicationCard";
+import ThemeCard from "./ThemeCard";
 
 export default function LaboPage(props) {
+  const [labo, setLabo] = useState({
+    nomLaboratoire: "",
+    responsable: {},
+    acronyme: "",
+    membresLabo: [],
+    themes: [],
+  });
+  const [publications, setPublications] = useState([]);
+  useEffect(() => {
+    const fetchDataLabo = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/FSTBM/Admin/Laboratoire/getById/${props.ident}`
+        );
+        setLabo(response.data);
+        console.log(labo);
+        const url = new URL("http://localhost:8080/FSTBM/scopus/publications");
+        url.searchParams.append("author", props.nomRespo);
+        fetch(url)
+          .then((res) => {
+            return res.json();
+          })
+          .then((data) => {
+            console.log(data);
+            setPublications(data);
+          });
+      } catch (error) {
+        console.log(error.response.data.message);
+        setLabo({});
+      }
+    };
 
-    const [labo,setLabo] = useState({nomLaboratoire:"",responsable:{},acronyme:"",membresLabo:[]});
-    const [publications,setPublications] = useState([]);
-    useEffect(() => {
-        const fetchDataLabo = async () => {
-          try {
-            const response = await axios.get(
-              `http://localhost:8080/FSTBM/Admin/Laboratoire/getById/${props.ident}`
-            );
-            setLabo(response.data);
-            console.log(labo)
-            const url = new URL("http://localhost:8080/FSTBM/scopus/publications");
-            url.searchParams.append("author", props.nomRespo);
-            fetch(url)
-              .then((res) => {
-                return res.json();
-              })
-              .then((data) => {
-                console.log(data);
-                setPublications(data);
-              });
-          } catch (error) {
-            console.log(error.response.data.message);
-            setLabo({});
-          }
-        };
-    
-        fetchDataLabo();
-      }, []);
+    fetchDataLabo();
+  }, []);
 
-
-  const [countDoctorant,setCountDoctorant] = useState();
-  useEffect(()=>{
+  const [countDoctorant, setCountDoctorant] = useState();
+  useEffect(() => {
     let doctorants = [];
-    labo.membresLabo.forEach((membre)=> {
-      doctorants.push(membre.doctorants)
-    })
-    setCountDoctorant(doctorants.length)
-  })
-
+    labo.membresLabo.forEach((membre) => {
+      doctorants.push(membre.doctorants);
+    });
+    setCountDoctorant(doctorants.length);
+  });
 
   const pubsRef = useRef();
+  const themeRef = useRef();
   const refMembres = useRef();
   const refDoctorants = useRef();
   const myRef = useRef();
   let minHeight = 2200;
   let minHeightMembre = 300;
   let minHeightDoctorants = 300;
+  let minHeightTheme = 100;
 
   let heightMembres = labo.membresLabo.length * 100;
   let heightDoctorants = countDoctorant * 100;
+  let heightThemes = labo.themes.length * 300;
   let heightPubs = publications.length * 260;
   let heightAdded = heightMembres + heightPubs + heightDoctorants;
   let newHeight = minHeight + heightAdded;
@@ -64,7 +71,6 @@ export default function LaboPage(props) {
   //   return <div>Loading...</div>;
   // }
 
-
   if (myRef.current) {
     myRef.current.style.height = newHeight + "px";
   }
@@ -72,19 +78,20 @@ export default function LaboPage(props) {
     pubsRef.current.style.height = heightPubs + "px";
   }
   if (refMembres.current) {
-    refMembres.current.style.height = (heightMembres+minHeightMembre) + "px";
+    refMembres.current.style.height = heightMembres + minHeightMembre + "px";
   }
   if (refDoctorants.current) {
-    refMembres.current.style.height = (heightDoctorants+minHeightDoctorants) + "px";
+    refMembres.current.style.height =
+      heightDoctorants + minHeightDoctorants + "px";
+  }
+  if (themeRef.current) {
+    themeRef.current.style.height = (minHeightTheme+heightThemes) + "px";
   }
 
 
   return (
     <div ref={myRef} className="labo-page-container bg-gray-100">
-      <div
-        className="image-back-labo"
-        
-      >
+      <div className="image-back-labo">
         <div className="pt-40 pl-24">
           <h1
             className="text-6xl max-xl:text-6xl p-3 text-white cursor-default hover:transition-colors w-fit bg-gray-500 bg-opacity-50"
@@ -114,7 +121,9 @@ export default function LaboPage(props) {
             className="w-1/3 rounded-md hover:bg-gray-50 bg-white shadow-sm h-24 mt-8 border border-gray-100"
           >
             <div className="mt-4 ml-10">
-              <h1 className="text-2xl" style={{fontFamily:"Roboto"}}>{labo.responsable.prenom} {labo.responsable.nom}</h1>
+              <h1 className="text-2xl" style={{ fontFamily: "Roboto" }}>
+                {labo.responsable.prenom} {labo.responsable.nom}
+              </h1>
               <p>{labo.responsable.email}</p>
             </div>
           </div>
@@ -131,9 +140,8 @@ export default function LaboPage(props) {
             Membres
           </h1>
         </div>
-        
-        <LaboMembre membresLabo={labo.membresLabo}/>
 
+        <LaboMembre membresLabo={labo.membresLabo} />
       </div>
       <div className="doctorant-equipe max-xl:mr-14" ref={refDoctorants}>
         <div style={{ marginLeft: "290px" }} className="flex mt-8 ml-52">
@@ -149,10 +157,37 @@ export default function LaboPage(props) {
           </h1>
         </div>
         <div>
-          <DoctorantLabo identLabo={props.ident}/>
+          <DoctorantLabo identLabo={props.ident} />
         </div>
       </div>
-      <div style={{ marginLeft: "290px",height:"40px" }} className="flex mt-40 ml-52">
+      <div style={{ marginLeft: "290px" }} className="flex mt-10 ml-52">
+        <div
+          style={{ borderLeftWidth: "14px" }}
+          className="h-15 border-l-yellow-400"
+        ></div>
+        <h1
+          style={{ fontFamily: "Roboto" }}
+          className="text-4xl pl-5 cursor-default"
+        >
+          Theme de recherches
+        </h1>
+      </div>
+      <div ref={themeRef} className="flex justify-center">
+        <div className="w-[2000px]">
+          {labo.themes.map((Theme, index) => (
+            <ThemeCard
+              key={index}
+              title={Theme.nomtheme}
+              desc={Theme.contentTheme}
+              image={Theme.imagePath}
+            />
+          ))}
+        </div>
+      </div>
+      <div
+        style={{ marginLeft: "290px", height: "40px" }}
+        className="flex mt-20 ml-52"
+      >
         <div
           style={{ borderLeftWidth: "14px" }}
           className="h-15 border-l-yellow-400"
